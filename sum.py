de79373a5732ce47053d7cbdb1cee691b8c3d376
ruteraga <ganzagabin1@gmail.com>
2025-07-29 16:21:04 +0200

    bruto-force approach. very bad time complexity

---
+++ sum.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def threeSum(self, nums):
+        dict=[]
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                l=[]
+                diff=0-(nums[i]+nums[j])
+                if diff in nums:
+                    k=nums.index(diff)
+                    if i!=j and i!=k and j!=k:
+                        l.append(nums[i])
+                        l.append(nums[j])
+                        l.append(nums[k])
+                l.sort()
+                if l not in dict and len(l)!=0:
+                    dict.append(l)
+        return dict
+
+sol=Solution()
+l=[-1,0,1,2,-1,-4]
+print(sol.threeSum(l))
